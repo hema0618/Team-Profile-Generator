c2a2125f3c5e47c6ce911fa68c6b8c0186889b3c
Hema <khemalatha507@gmail.com>
2020-08-18 17:36:11 -0400

    validate the questions

--- Develop/app.js
+++ Develop/app.js
@@ -43,93 +43,100 @@ function createTeam() {
                 return console.log("HTML has succesfully been created!");
             }
         });
-    }
-            }
+}
+function askQuestions() {
+    inquirer.prompt([
+        {
+            type: "list",
+            choices: ["Manager", "Engineer", "Intern"],
+            message: "What is your role in the company?",
+            name: "role",
+        },
+        {
+            type: "input",
+            message: "What is your full name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is your ID number?",
+            name: "id",
+        },
+     
+    ])
+    .then(function(response){
+        this.name = response.name;
+        this.id = response.id;
+        this.role = response.role;  
+    
+        function checkEmail() {
             inquirer.prompt([
                 {
-                    type: "list",
-                    choices: ["Manager", "Engineer", "Intern"],
-                    message: "What is your role in the company?",
-                    name: "position",
-                },
-                {
-                    type: "input",
-                    message: "What is your full name?",
-                    name: "name",
-                },
-                {
-                    type: "input",
-                    message: "What is your ID number?",
-                    name: "id",
-                },
-                {
-                    type: "input",
-                    message: "What is your email address?",
+                    type: "email",
+                    message:"What is your email address?",
                     name: "email",
-                },
-            ]).then(function (response) {
-                this.position = response.position;
-                this.name = response.name;
-                this.id = response.id;
-                this.email = response.email;
-                if (this.position === "Manager") {
-                    inquirer.prompt([
-                        {
-                            type: "input",
-                            message: "What is your office number?",
-                            name: "officeNumber",
-                        }
-                    ]).then(function (response) {
-                        fs.appendFile("main.html", new Manager(response), function (error) {
-                            if (error) {
-                                console.log(error);
-                                return;
-                            }
-                            console.log("Success! The team has a manager.");
-                        })
-                    })
-                } else if (this.position === "Engineer") {
-                    inquirer.prompt([
-                        {
-                            type: "input",
-                            message: "What is your GitHub account name?",
-                            name: "GitHub",
-                        }
-                    ]).then(function (response) {
-                        fs.appendFile("engineer.html", new Engineer(response), function (error) {
-                            if (error) {
-                                console.log(error);
-                                return;
-                            }
-                            console.log("Success! Added an Engineer to the team.");
-                        })
-                    })
-                } else if (this.position === "intern") {
-                    inquirer.prompt([
-                        {
-                            type: "input",
-                            message: "What is your GitHub account name?",
-                            name: "GitHub",
-                        }
-                    ]).then(function (response) {
-                        fs.appendFile("intern.html", Intern(response), function (error) {
-                            if (error) {
-                                console.log(error);
-                                return;
-                            }
-                            console.log("Success! Added an intern to the team.");
-                        })
-                    })
                 }
-
+             ]).then(function(response){
+                this.email = response.email;
+                if (validator.validate(this.email)) {
+                    this.email = response.email;
+                    if (this.role === "Manager") {
+                        inquirer.prompt([
+                            {
+                                type: "input",
+                                message: "What is your office number?",
+                                name: "officeNumber",
+                            }
+                        ]).then(function(response) {
+                                this.officeNumber = response.officeNumber;
+                                const newManager = new Manager(this.name, this.id, this.email, this.officeNumber);
+                                console.log("Success! The team has a manager.");
+                                team.push(newManager);
+                            }).then(function() {
+                                addMore();
+                            });
+                    } else if (this.role === "Engineer") {
+                        inquirer.prompt([
+                            {
+                                type: "input",
+                                message: "What is your GitHub account name?",
+                                name: "GitHub",
+                            }
+                        ]).then(function(response) {
+                                this.Github = response.Github;
+                                const newEngineer = new Engineer(this.name, this.id, this.email, this.Github);
+                                console.log("Success! Added an Engineer to the team.");
+                                team.push(newEngineer);
+                            }).then(function() {
+                                addMore();
+                            });
+                    } else if (this.role === "Intern") {
+                        inquirer.prompt([
+                            {
+                                type: "input",
+                                message: "What college did you go to?",
+                                name: "school",
+                            }
+                        ]).then(function(response) {
+                            this.school = response.school;
+                            const newIntern = new Intern(this.name, this.id, this.email, this.school);
+                            console.log("Success! Added an intern to the team.");
+                            team.push(newIntern);
+                            }).then(function() {
+                                addMore();
+                            });
+                        
+                        };
+                } else {
+                    console.log("Please enter a valid email address.");
+                    checkEmail();
+                }
             })
-
-
-
-            const newEmployee = new Employee(response);
-
-
-
+        };
+        checkEmail();
+        });
+    };
+    askQuestions();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
